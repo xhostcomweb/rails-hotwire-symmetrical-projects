5134254e46fa168969426bcb468f10677ad59ecc
xhostcom <xhostcom@gmail.com>
2022-04-07 06:53:29 +0100

    Gitacora saved 2 changed files

--- db/migrate/20220406200633_create_projects.rb
+++ db/migrate/20220406200633_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
       t.string :content
-      t.integer :vote
+      t.integer :vote, default: 0, null: false
 
       t.timestamps
     end
